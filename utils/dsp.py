71ec6738d9079142db398a7c1655fd4d4c0a8f7e
NA\truxonjm <truxonjm@corning.com>
2023-01-07 21:58:40 -0500

    bugfix re:changes in librosa design

--- utils/dsp.py
+++ utils/dsp.py
@@ -1,17 +1,17 @@
 import math
 import numpy as np
-import librosa
+import librosa, soundfile
 from utils import hparams as hp
 from scipy.signal import lfilter
 
 
 def label_2_float(x, bits):
-    return 2 * x / (2**bits - 1.) - 1.
+    return 2 * x / (2**bits - 1.0) - 1.0
 
 
 def float_2_label(x, bits):
     assert abs(x).max() <= 1.0
-    x = (x + 1.) * (2**bits - 1) / 2
+    x = (x + 1.0) * (2**bits - 1) / 2
     return x.clip(0, 2**bits - 1)
 
 
@@ -20,7 +20,7 @@ def load_wav(path):
 
 
 def save_wav(x, path):
-    librosa.output.write_wav(path, x.astype(np.float32), sr=hp.sample_rate)
+    soundfile.write(path, x.astype(np.float32), sr=hp.sample_rate)
 
 
 def split_signal(x):
@@ -35,17 +35,24 @@ def combine_signal(coarse, fine):
 
 
 def encode_16bits(x):
-    return np.clip(x * 2**15, -2**15, 2**15 - 1).astype(np.int16)
+    return np.clip(x * 2**15, -(2**15), 2**15 - 1).astype(np.int16)
 
 
 def linear_to_mel(spectrogram):
     return librosa.feature.melspectrogram(
-        S=spectrogram, sr=hp.sample_rate, n_fft=hp.n_fft, n_mels=hp.num_mels, fmin=hp.fmin)
+        S=spectrogram,
+        sr=hp.sample_rate,
+        n_fft=hp.n_fft,
+        n_mels=hp.num_mels,
+        fmin=hp.fmin,
+    )
 
-'''
+
+"""
 def build_mel_basis():
     return librosa.filters.mel(hp.sample_rate, hp.n_fft, n_mels=hp.num_mels, fmin=hp.fmin)
-'''
+"""
+
 
 def normalize(S):
     return np.clip((S - hp.min_level_db) / -hp.min_level_db, 0, 1)
@@ -77,8 +84,8 @@ def melspectrogram(y):
 
 def stft(y):
     return librosa.stft(
-        y=y,
-        n_fft=hp.n_fft, hop_length=hp.hop_length, win_length=hp.win_length)
+        y=y, n_fft=hp.n_fft, hop_length=hp.hop_length, win_length=hp.win_length
+    )
 
 
 def pre_emphasis(x):
@@ -97,10 +104,12 @@ def encode_mu_law(x, mu):
 
 def decode_mu_law(y, mu, from_labels=True):
     # TODO: get rid of log2 - makes no sense
-    if from_labels: y = label_2_float(y, math.log2(mu))
+    if from_labels:
+        y = label_2_float(y, math.log2(mu))
     mu = mu - 1
     x = np.sign(y) / mu * ((1 + mu) ** np.abs(y) - 1)
     return x
+
 
 def reconstruct_waveform(mel, n_iter=32):
     """Uses Griffin-Lim phase reconstruction to convert from a normalized
@@ -108,10 +117,9 @@ def reconstruct_waveform(mel, n_iter=32):
     denormalized = denormalize(mel)
     amp_mel = db_to_amp(denormalized)
     S = librosa.feature.inverse.mel_to_stft(
-        amp_mel, power=1, sr=hp.sample_rate,
-        n_fft=hp.n_fft, fmin=hp.fmin)
+        amp_mel, power=1, sr=hp.sample_rate, n_fft=hp.n_fft, fmin=hp.fmin
+    )
     wav = librosa.core.griffinlim(
-        S, n_iter=n_iter,
-        hop_length=hp.hop_length, win_length=hp.win_length)
+        S, n_iter=n_iter, hop_length=hp.hop_length, win_length=hp.win_length
+    )
     return wav
-
